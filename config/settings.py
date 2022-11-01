e708c8662d943d31e6a54ee61241b0c5c841b366
berdibaevich <berdibaevichjetker@gmail.com>
2022-11-01 22:23:54 +0500

    commited

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 #for css
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATIC_ROOT = str(BASE_DIR.joinpath('static'))
 
 
 
